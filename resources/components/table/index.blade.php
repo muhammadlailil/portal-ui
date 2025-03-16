e8326bb5418f38f484efc1e4b24a307758d052af
laililmahfud <laililmahvut@gmail.com>
2025-03-16 22:24:46 +0700

    table ui

--- resources/components/table/index.blade.php
+++ resources/components/table/index.blade.php
@@ -1,9 +1,7 @@
-<div {{ $attributes->merge(['class' => 'overflow-x-auto rounded-md w-full bg-background border rounded-md']) }} >
-    <div class="inline-block min-w-full box-table">
-        <div class="overflow-hidden">
-            <table class="min-w-full divide-y divide-border">
-                {{ $slot }}
-            </table>
-        </div>
+<div {{ $attributes->merge(['class' => 'overflow-x-auto rounded-md w-full bg-background border rounded-md']) }}>
+    <div class="min-w-full box-table">
+        <table class="min-w-full divide-y divide-border">
+            {{ $slot }}
+        </table>
     </div>
 </div>
